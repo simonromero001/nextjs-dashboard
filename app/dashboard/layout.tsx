faf29c451f63bbd1ae46886f331452d9d9005ba0
Simon Romero <simon.romero001@gmail.com>
2024-03-02 22:18:25 -0800

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import React from 'react';
 
-export default function Layout({ children }: { children: React.ReactDOM }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
